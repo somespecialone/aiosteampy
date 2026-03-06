8a6e2be50dfc3030a52df0318a711e2a88e0ea73
somespecialone <tkachenkodmitriy@yahoo.com>
2026-03-06 21:22:41 +0100

    Removed unused async throttle util

--- aiosteampy/utils.py
+++ aiosteampy/utils.py
@@ -1,19 +1,7 @@
 """Abstract utils within `Steam` context and not"""
 
-import asyncio
-import time
 import re
-
-from functools import wraps
-from typing import Callable, overload, TypeVar
-
-
-__all__ = (
-    "extract_openid_payload",
-    "async_throttle",
-    "create_ident_code",
-    "extract_icon_hash_from_app_icon_link",
-)
+from typing import overload
 
 
 def extract_openid_payload(page_text: str) -> dict[str, str]:
@@ -32,62 +20,6 @@ def extract_openid_payload(page_text: str) -> dict[str, str]:
         "openidparams": re.search(r"name=\"openidparams\"[\w=\"\s]+value=\"(?P<params>[\w=/]+)\"", page_text)["params"],
         "nonce": re.search(r"name=\"nonce\"[\w=\"\s]+value=\"(?P<nonce>\w+)\"", page_text)["nonce"],
     }
-
-
-_R = TypeVar("_R")
-
-
-def async_throttle(
-    seconds: float,
-    *,
-    arg_index: int | None = None,
-    arg_name: str | None = None,
-) -> Callable[[Callable[..., _R]], Callable[..., _R]]:
-    """
-    Prevents the decorated function from being called more than once per ``seconds``.
-    Throttle (`await asyncio.sleep`) before call wrapped async func,
-    when related arg was equal (same hash value) to related arg passed in previous func call
-    if time between previous and current call lower than ``seconds``.
-    Related arg must be *hashable* (can be dict key).
-    Wrapped func must be *async* (return ``Coroutine``).
-    Throttle every func call time if related arg has been not specified.
-
-    :param seconds: seconds during which call frequency has been limited.
-    :param arg_index: index of related arg in ``*args`` tuple.
-    :param arg_name: keyname of related arg in ``**kwargs`` dict.
-    """
-
-    if arg_index is not None and arg_name is not None:
-        raise ValueError("You can't specify both `arg_index` and `arg_name`")
-
-    # mega optimization, prevent arg checks in wrapped func call
-    # I know about PEP8: E731, but this way is much shorter and readable
-    if arg_index is None and arg_name is None:
-        get_key = lambda _, __: None
-    elif arg_index:
-        get_key = lambda a, _: a[arg_index]
-    else:
-        get_key = lambda _, k: k[arg_name]
-
-    ts_map: dict[..., float] = {}
-
-    def decorator(f: Callable[..., _R]) -> Callable[..., _R]:
-        @wraps(f)
-        async def wrapper(*args, **kwargs) -> _R:
-            key = get_key(args, kwargs)
-            ts = time.time()
-            diff = ts - ts_map.get(key, 0)
-            if diff < seconds:
-                await asyncio.sleep(diff + 0.01)  # +10ms to ensure that second passed since last call
-                ts_map[key] = time.time()
-            else:
-                ts_map[key] = ts
-
-            return await f(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
 
 
 # TODO remove overloading and write examples in docstring: args should be from most narrow to wide (inst, class, app)

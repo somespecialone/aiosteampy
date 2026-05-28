6a84cdb6e2d4c87abbaa8619adaf9380988d07df
somespecialone <itsme@somespecial.one>
2026-05-28 15:28:06 +0200

    feat(CS2): typed market listings query

--- aiosteampy/client/cs2.py
+++ aiosteampy/client/cs2.py
@@ -1,13 +1,15 @@
 """`CS2` app specific context."""
 
 import re
-from dataclasses import dataclass
+from collections.abc import Iterable
+from dataclasses import dataclass, field
 from enum import IntEnum, StrEnum
-from typing import TYPE_CHECKING, NamedTuple, Self
+from typing import TYPE_CHECKING, Literal, NamedTuple, Self
 
 import betterproto2
 
 from .app import App
+from .components.market.query import ListingsQuery
 
 if TYPE_CHECKING:  # break circle import
     from .components.market import ListingItem, MarketListingItem
@@ -189,7 +191,7 @@ class CEconItemPreviewDataBlock(betterproto2.Message):
         return cls.parse(buffer[1:-4])
 
 
-@dataclass(repr=False)
+@dataclass(eq=False, repr=False)
 class CEconItemPreviewDataBlockSticker(betterproto2.Message):
     slot: "int" = betterproto2.field(1, betterproto2.TYPE_UINT32)
     sticker_id: "int" = betterproto2.field(2, betterproto2.TYPE_UINT32)
@@ -205,6 +207,7 @@ class CEconItemPreviewDataBlockSticker(betterproto2.Message):
     wrapped_sticker: "int" = betterproto2.field(12, betterproto2.TYPE_UINT32)
 
 
+# can this and preview(inspect data) be merged into something unified?
 @dataclass(slots=True)
 class ItemContext:
     """Representation of `CS2` specific ``EconItem`` data."""
@@ -297,3 +300,264 @@ class ItemContext:
                     name_tag = prop.value
 
         return cls(descr_ctx, stickers, charm, inspect_id, wear_rating, pattern, name_tag)
+
+
+TExteriors = Literal["Factory New", "Minimal Wear", "Field-Tested", "Well-Worn", "Battle-Scarred", "Not Painted"]
+
+TQualities = Literal[
+    "Normal",
+    "Souvenir",
+    "StatTrak™",
+    "★",  # unused
+    "★ StatTrak™",  # simple stattrak can be used instead
+    "Highlight",  # ?
+    "Customized",  # ?
+]
+
+TTypes = Literal[
+    "SMG",
+    "Pistol",
+    "Rifle",
+    "Sniper Rifle",
+    "Shotgun",
+    "Machinegun",
+    "Charm",
+    "Equipment",
+    "Knife",
+    "Agent",
+    "Container",
+    "Sticker",
+    "Gloves",
+    "Graffiti",
+    "Music Kit",
+    "Patch",
+    "Collectible",
+    "Pass",
+    "Key",
+    "Gift",
+    "Tag",
+    "Tool",
+]
+
+TWeapons = Literal[
+    "MP9",
+    "PP-Bizon",
+    "SSG 08",
+    "P90",
+    "P250",
+    "SCAR-20",
+    "Nova",
+    "MAC-10",
+    "UMP-45",
+    "Tec-9",
+    "G3SG1",
+    "MP5-SD",
+    "MAG-7",
+    "SG 553",
+    "Sawed-Off",
+    "MP7",
+    "Dual Berettas",
+    "R8 Revolver",
+    "FAMAS",
+    "AUG",
+    "Five-SeveN",
+    "AK-47",
+    "Galil AR",
+    "M249",
+    "CZ75-Auto",
+    "XM1014",
+    "M4A1-S",
+    "Negev",
+    "Desert Eagle",
+    "USP-S",
+    "Zeus x27",
+    "M4A4",
+    "Glock-18",
+    "AWP",
+    "P2000",
+    "Kukri Knife",
+    "Shadow Daggers",
+    "Bowie Knife",
+    "Falchion Knife",
+    "Survival Knife",
+    "Huntsman Knife",
+    "Paracord Knife",
+    "Navaja Knife",
+    "Gut Knife",
+    "Nomad Knife",
+    "Skeleton Knife",
+    "Ursus Knife",
+    "Bayonet",
+    "Flip Knife",
+    "Butterfly Knife",
+    "Stiletto Knife",
+    "Talon Knife",
+    "Classic Knife",
+    "M9 Bayonet",
+    "Karambit",
+]
+
+TStickerTypes = Literal["Player Autograph", "Team Logo", "Tournament"]
+
+# localized: value
+TAGS_MAP = {
+    "AK-47": "weapon_ak47",
+    "AUG": "weapon_aug",
+    "AWP": "weapon_awp",
+    "Agent": "Type_CustomPlayer",
+    "Battle-Scarred": "WearCategory4",
+    "Bayonet": "weapon_bayonet",
+    "Bowie Knife": "weapon_knife_survival_bowie",
+    "Butterfly Knife": "weapon_knife_butterfly",
+    "CZ75-Auto": "weapon_cz75a",
+    "Charm": "CSGO_Tool_Keychain",
+    "Classic Knife": "weapon_knife_css",
+    "Collectible": "CSGO_Type_Collectible",
+    "Container": "CSGO_Type_WeaponCase",
+    "Customized": "customized",
+    "Desert Eagle": "weapon_deagle",
+    "Dual Berettas": "weapon_elite",
+    "Equipment": "CSGO_Type_Equipment",
+    "FAMAS": "weapon_famas",
+    "Factory New": "WearCategory0",
+    "Falchion Knife": "weapon_knife_falchion",
+    "Field-Tested": "WearCategory2",
+    "Five-SeveN": "weapon_fiveseven",
+    "Flip Knife": "weapon_knife_flip",
+    "G3SG1": "weapon_g3sg1",
+    "Galil AR": "weapon_galilar",
+    "Gift": "CSGO_Tool_GiftTag",
+    "Glock-18": "weapon_glock",
+    "Gloves": "Type_Hands",
+    "Graffiti": "CSGO_Type_Spray",
+    "Gut Knife": "weapon_knife_gut",
+    "Highlight": "highlight",
+    "Huntsman Knife": "weapon_knife_tactical",
+    "Karambit": "weapon_knife_karambit",
+    "Key": "CSGO_Tool_WeaponCase_KeyTag",
+    "Knife": "CSGO_Type_Knife",
+    "Kukri Knife": "weapon_knife_kukri",
+    "M249": "weapon_m249",
+    "M4A1-S": "weapon_m4a1_silencer",
+    "M4A4": "weapon_m4a1",
+    "M9 Bayonet": "weapon_knife_m9_bayonet",
+    "MAC-10": "weapon_mac10",
+    "MAG-7": "weapon_mag7",
+    "MP5-SD": "weapon_mp5sd",
+    "MP7": "weapon_mp7",
+    "MP9": "weapon_mp9",
+    "Machinegun": "CSGO_Type_Machinegun",
+    "Minimal Wear": "WearCategory1",
+    "Music Kit": "CSGO_Type_MusicKit",
+    "Navaja Knife": "weapon_knife_gypsy_jackknife",
+    "Negev": "weapon_negev",
+    "Nomad Knife": "weapon_knife_outdoor",
+    "Normal": "normal",
+    "Not Painted": "WearCategoryNA",
+    "Nova": "weapon_nova",
+    "P2000": "weapon_hkp2000",
+    "P250": "weapon_p250",
+    "P90": "weapon_p90",
+    "PP-Bizon": "weapon_bizon",
+    "Paracord Knife": "weapon_knife_cord",
+    "Pass": "CSGO_Type_Ticket",
+    "Patch": "CSGO_Tool_Patch",
+    "Pistol": "CSGO_Type_Pistol",
+    "R8 Revolver": "weapon_revolver",
+    "Rifle": "CSGO_Type_Rifle",
+    "SCAR-20": "weapon_scar20",
+    "SG 553": "weapon_sg556",
+    "SMG": "CSGO_Type_SMG",
+    "SSG 08": "weapon_ssg08",
+    "Sawed-Off": "weapon_sawedoff",
+    "Shadow Daggers": "weapon_knife_push",
+    "Shotgun": "CSGO_Type_Shotgun",
+    "Skeleton Knife": "weapon_knife_skeleton",
+    "Sniper Rifle": "CSGO_Type_SniperRifle",
+    "Souvenir": "tournament",
+    "StatTrak™": "strange",
+    "Sticker": "CSGO_Tool_Sticker",
+    "Stiletto Knife": "weapon_knife_stiletto",
+    "Survival Knife": "weapon_knife_canis",
+    "Tag": "CSGO_Tool_Name_TagTag",
+    "Talon Knife": "weapon_knife_widowmaker",
+    "Tec-9": "weapon_tec9",
+    "Tool": "CSGO_Type_Tool",
+    "UMP-45": "weapon_ump45",
+    "USP-S": "weapon_usp_silencer",
+    "Ursus Knife": "weapon_knife_ursus",
+    "Well-Worn": "WearCategory3",
+    "XM1014": "weapon_xm1014",
+    "Zeus x27": "weapon_taser",
+    "★": "unusual",
+    "★ StatTrak™": "unusual_strange",
+}
+
+
+@dataclass(slots=True, kw_only=True)
+class CS2SearchQuery(ListingsQuery):
+    """
+    ``CS2`` specific query builder for `listings`,
+    uses `localized` names for methods and values.
+    Non-exhaustive, so does not contain every possible filter typed.
+
+    .. note:: This builder does not prevent a wrong filters combination,
+        so it is strongly advised to be vigilant.
+    """
+
+    # anyway app need to be passed in component methods
+    # better to be redesigned
+    app: App = field(default_factory=lambda: App.CS2)
+
+    def _add_filters(self, facet: str, values: Iterable[str]):
+        for value in values:
+            self.filter(facet, TAGS_MAP[value])
+
+    def _add_accessories(self, facet: str, values: Iterable[str]):
+        for value in values:
+            self.accessory(facet, value)
+
+    def exterior(self, *values: TExteriors | ItemExterior) -> Self:
+        """Add ``exterior`` filter."""
+        self._add_filters("Exterior", (str(v) for v in values))
+        return self
+
+    def category(self, *values: TQualities) -> Self:
+        """Add ``category`` filter."""
+        self._add_filters("Quality", values)
+        return self
+
+    def type(self, *values: TTypes) -> Self:
+        """Add ``type`` filter."""
+        self._add_filters("Type", values)
+        return self
+
+    def weapon(self, *values: TWeapons) -> Self:
+        """Add ``weapon`` filter."""
+        self._add_filters("Weapon", values)
+        return self
+
+    def sticker_type(self, *values: TStickerTypes) -> Self:
+        """Add ``sticker_type`` filter."""
+        self._add_filters("StickerCategory", values)
+        return self
+
+    def sticker(self, *values: str) -> Self:
+        """Add attached ``sticker`` to item as filter. Value must be `sticker` full name(market hash name)."""
+        self._add_accessories(TAGS_MAP["Sticker"], values)
+        return self
+
+    def charm(self, *values: str) -> Self:
+        """Add attached ``charm`` to item as filter. Value must be `keychain` full name(market hash name)."""
+        self._add_accessories(TAGS_MAP["Charm"], values)
+        return self
+
+    def wear_rating(self, min_: float = 0, max_: float = 1) -> Self:
+        """Set `wear rating (ala floatvalue)` range as filter."""
+        self.property(2, float_min=min_, float_max=max_)
+        return self
+
+    def pattern(self, min_: int = 0, max_: int = 1000) -> Self:
+        """Set `pattern template (paint seed)` range as filter."""
+        self.property(1, int_min=min_, int_max=max_)  # browser sends values as string, hah
+        return self
